4d325ffda64d19570c3c23f6274fdae6f437d0fd
Mike Henry <chsmike@gmail.com>
2015-10-07 13:23:07 -0400

    Initial commit

---
+++ Landmark/Landmark/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Landmark
+//
+//  Created by Mike Henry on 10/7/15.
+//  Copyright © 2015 Mike Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface ViewController : UIViewController <MKMapViewDelegate, CLLocationManagerDelegate, UISearchBarDelegate>
+
+
+@end
+
---
+++ Landmark/Landmark/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  Landmark
+//
+//  Created by Mike Henry on 10/7/15.
+//  Copyright © 2015 Mike Henry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
